44bf9c20877818b932afff14f92127ffeabb39a7
Nate Kane <74371715+Nate-Kane@users.noreply.github.com>
2025-06-06 17:47:45 -0600

    Footer

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,12 +1,42 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const FooterContainer = styled.footer`
+  width: 100%;
+  color: var(--primary-color, #fff);
+  text-align: center;
+  font-size: var(--font-size-xs, 1rem);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 0px;
+`;
+
+const FooterLinks = styled.div`
+  margin-left: 20px;
+  a {
+    color: var(--primary-color, #fff);
+    text-decoration: none;
+    font-weight: 600;
+    transition: color 0.2s;
+    &:hover {
+      color: rgb(52, 74, 134);
+    }
+  }
+`;
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <h2>Footer Section</h2>
-      </div>
-    </footer>
+    <FooterContainer>
+      <div>© {new Date().getFullYear()} Nate Kane</div>
+      <FooterLinks>
+        <a href="mailto:ndkane3@gmail.com">ndkane3@gmail.com</a>
+        &nbsp;|&nbsp;
+        <a href="https://github.com/Nate-Kane" target="_blank" rel="noopener noreferrer">GitHub</a>
+        &nbsp;|&nbsp;
+        <a href="https://www.linkedin.com/in/nathandkane/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </FooterLinks>
+    </FooterContainer>
   );
 };
 
